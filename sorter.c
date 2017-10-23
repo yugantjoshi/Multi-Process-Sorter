4c997cf4ff88bf141eedf42b71068e4ee9258821
Yugant Joshi <joshi.yugant@gmail.com>
2017-10-23 13:02:07 -0400

    small changes

--- sorter.c
+++ sorter.c
@@ -255,6 +255,30 @@ char* findColumnType(const char* value){
   }
 }
 
+void listdir(const char *name, int indent)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(name)))
+        return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            char path[1024];
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+            printf("%*s[%s]\n", indent, "", entry->d_name);
+            listdir(path, indent + 2);
+        } else {
+            printf("%*s- %s\n", indent, "", entry->d_name);
+        }
+    }
+    closedir(dir);
+}
+
+
 int main(int argc, char const *argv[]) {
   Records* input = (Records*)malloc(sizeof(Records)*8000);
 
--- sorter.h
+++ sorter.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <dirent.h>
 #include <ctype.h>
 #include <math.h>
 
